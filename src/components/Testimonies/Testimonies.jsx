0fa4a0e064e88eb7a35c313079d637697ca09f14
Gaimar23 <highdevelopment01@gmail.com>
2024-10-02 07:40:06 +0200

    sixth commit

--- src/components/Testimonies/Testimonies.jsx
+++ src/components/Testimonies/Testimonies.jsx
@@ -2,38 +2,84 @@ import React from "react";
 import "./Testimonies.scss";
 import { IoIosStar } from "react-icons/io";
 import { IoIosStarHalf } from "react-icons/io";
+import profil from "../../assets/images/1.jpg";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Testimonies = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+  };
   return (
     <div className="testimonies-container">
       <div className="sub-container">
         <div className="left">
-          <div className="card-container">
-            <div className="item">
-              <div className="identification">
-                <div className="name-country">
-                  <span className="name">Frederic Samo</span>
-                  <h4 className="country">Cameroun</h4>
+          <div className="slider-container">
+            <Slider {...settings} className="testimonies-list">
+              <div className="item-container">
+                <div className="item">
+                  <div className="identification">
+                    <div className="name-country">
+                      <span className="name">Frederic Samo</span>
+                      <h4 className="country">Cameroun</h4>
+                    </div>
+                    <div className="age">35 ans</div>
+                  </div>
+                  <div className="message">
+                    J'ai réussi mon projet d'immigration du cameroun pour le
+                    Canada il y a 1.5 an grâce à AIDE CONSULTING qui m'a assité
+                    à toutes les étapes du processus. Alors Un Bravo à l'équipe
+                    AIDE CONSULTING
+                  </div>
+                  <div className="stars">
+                    <IoIosStar className="star-icon" />
+                    <IoIosStar className="star-icon" />
+                    <IoIosStar className="star-icon" />
+                    <IoIosStar className="star-icon" />
+                    <IoIosStar className="star-icon" />
+                  </div>
+                  <div className="image-container">
+                    <img src={profil} alt="" />
+                  </div>
                 </div>
-                <div className="age">35 ans</div>
               </div>
-              <div className="message">
-                J'ai réussi mon projet d'immigration du cameroun pour le Canada
-                il y a 1.5 an grâce à AIDE CONSULTING qui m'a assité à toutes
-                les étapes du processus. Alors Un Bravo à l'équipe AIDE
-                CONSULTING
+              <div className="item-container">
+                <div className="item">
+                  <div className="identification">
+                    <div className="name-country">
+                      <span className="name">Frederic Samo</span>
+                      <h4 className="country">Cameroun</h4>
+                    </div>
+                    <div className="age">35 ans</div>
+                  </div>
+                  <div className="message">
+                    J'ai réussi mon projet d'immigration du cameroun pour le
+                    Canada il y a 1.5 an grâce à AIDE CONSULTING qui m'a assité
+                    à toutes les étapes du processus. Alors Un Bravo à l'équipe
+                    AIDE CONSULTING
+                  </div>
+                  <div className="stars">
+                    <IoIosStar className="star-icon" />
+                    <IoIosStar className="star-icon" />
+                    <IoIosStar className="star-icon" />
+                    <IoIosStar className="star-icon" />
+                    <IoIosStar className="star-icon" />
+                  </div>
+                  <div className="image-container">
+                    <img src={profil} alt="" />
+                  </div>
+                </div>
               </div>
-              <div className="stars">
-                <IoIosStar className="star-icon" />
-                <IoIosStar className="star-icon" />
-                <IoIosStar className="star-icon" />
-                <IoIosStar className="star-icon" />
-                <IoIosStar className="star-icon" />
-              </div>
-              <div className="image-container">
-                <img src="" alt="" />
-              </div>
-            </div>
+            </Slider>
           </div>
         </div>
         <div className="right">
